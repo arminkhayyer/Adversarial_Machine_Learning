f9a306be8881bc499fdcb2025b1a2cc8bb15be87
Bhargav <bhargav.csse@gmail.com>
2019-10-18 19:37:06 -0500

    FFNN-updateV1.0

--- Project-3/FFNN_B.py
+++ Project-3/FFNN_B.py
@@ -5,59 +5,53 @@ Created on Tue Oct 15 18:09:20 2019
 
 @author: bhargavjoshi
 """
+
 #Library imports
 import math
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as pyplt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import sklearn
 from sklearn.neural_network import MLPClassifier
 from sklearn.neural_network import MLPRegressor
 
 #Tools import
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
-class aFFNeuralNetwork:
-    def __init__(self, filename, trainRatio, testRatio):
-        self.filename = str(filename)
-        self.desiredTarget = []
-        self.inputFeatures = []
-        self.trainRatio = trainRatio
-        self.testRatio = testRatio
-        self.desiredTarget_train = []
-        self.desiredTarget_test = []
-        self.inputFeatures_train = []
-        self.inputFeatures_test = []
-        self.inputFeatures_train_scale = []
-        self.inputFeatures_test_scale = []
-        
-    def ReadDataset(self):
-        dataset = pd.read_csv(self.filename,sep=' ', header=None, names = ["Feature_1","Feature_2","Output"])
-        self.inputFeatures = dataset.drop("Output", axis=1)
-        self.desiredTarget = dataset["Output"]
-        
-    def GenerateTrainTestSamples(self):
-        inputFeatures_train, inputFeature_test, self.desiredTarget_train, self.desiredTarget_test = train_test_split(self.inputFeatures, 
-                                                                                                           self.desiredTarget, 
-                                                                                                           train_size = self.trainRatio, 
-                                                                                                           test_size = self.testRatio, 
-                                                                                                           shuffle = True)
-        scaler = StandardScaler()
-        scaler.fit(inputFeatures_train)
-        self.inputFeatures_train_scale = scaler.transform(inputFeatures_train)
-        self.inputFeatures_test_scale = scaler.transform(inputFeature_test) 
-        
-    def DeployNeuralNetwork(self, iterations, hiddenLayers):
-        self.ReadDataset()
-        self.GenerateTrainTestSamples()
-        mlp = MLPClassifier(hidden_layer_sizes = hiddenLayers, max_iter = iterations)
-        mlp.fit(self.inputFeatures_train_scale, self.desiredTarget_train)
-        prediction = mlp.predict(self.inputFeatures_test_scale)
-        print(confusion_matrix(self.desiredTarget_test,prediction))
-        
+#scaler = StandardScaler()
+#scaler.fit(self.inputFeatures_train)
+#self.inputFeatures_train_scale = scaler.transform(self.inputFeatures_train)
+#self.inputFeatures_test_scale = scaler.transform(self.inputFeature_test)
 
-        
-NeuralNetwork1 = aFFNeuralNetwork("Project3_Dataset_v1.txt", trainRatio = 0.7, testRatio = 0.3)
-NeuralNetwork1.DeployNeuralNetwork(iterations = 100, hiddenLayers = [10,10,10])
+dataset = pd.read_csv("Project3_Dataset_v1.txt",sep=' ', header=None)
+DS_array = dataset.to_numpy()
+X = DS_array[:,0:2]
+y = DS_array[:,2]
+
+clf = MLPRegressor(solver='sgd', alpha=1e-5, hidden_layer_sizes=(10, 5), max_iter = 100000, learning_rate_init = 0.05)
+clf.fit(X,y)
+predictions = clf.predict(X)
+MSE = mean_squared_error(y, predictions)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1, projection='3d')
+ax1.scatter(X[:,0], X[:,1], y)
+plt.title("Evolved Candidate Solutions")
+ax1.set_xlim3d(-100.0, 100.0)
+ax1.set_ylim3d(-100.0, 100.0)
+ax1.set_zlim3d(0, 1.0)
+plt.show()
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(1, 1, 1, projection='3d')
+ax1.scatter(X[:,0], X[:,1], predictions)
+plt.title("Evolved Candidate Solutions")
+ax1.set_xlim3d(-100.0, 100.0)
+ax1.set_ylim3d(-100.0, 100.0)
+ax1.set_zlim3d(0, 1.0)
+plt.show()
+
+print("AMSE: "+str(MSE))
