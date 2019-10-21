7d91c79ff562aaf21609a20eb14a59b513b9cc7b
ye <yewang@auburn.edu>
2019-10-20 23:36:40 -0500

    update svm

--- Project-3/SVM.py
+++ Project-3/SVM.py
@@ -5,18 +5,20 @@ from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 
 
+from mpl_toolkits.mplot3d.axes3d import get_test_data
 x_ = np.loadtxt('Project3_Dataset_v2.txt')
 train = math.floor(0.7 * x_.shape[0])
 eval = math.floor(.8 * x_.shape[0])
 test = math.floor(.9 * x_.shape[0])
 
-x_train = x_[:train, 0:2]
-y_train = x_[:train, 2]
+x_train = x_[:eval, 0:2]
+y_train = x_[:eval, 2]
 
-x_test = x_[train:, 0:2]
-y_test = x_[train:, 2]
+x_test = x_[eval:, 0:2]
+y_test = x_[eval:, 2]
 
 kernel='rbf'
+
 
 if kernel =='linear':
     clf = LinearSVC(random_state=0, tol=1e-4)
@@ -27,6 +29,7 @@ if kernel == 'rbf':
 
 clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
+
 
 tp = 0
 tn = 0
@@ -42,24 +45,6 @@ for test_instance_result, label in zip(y_pred, y_test):
         fp += 1
     if ((test_instance_result <= 0.5) and (label > 0.5)):
         fn += 1
-
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 1, 1, projection='3d')
-ax1.scatter(x_test[:,0], x_test[:,1], y_test)
-plt.title("Evolved Candidate Solutions")
-ax1.set_xlim3d(-100.0, 100.0)
-ax1.set_ylim3d(-100.0, 100.0)
-ax1.set_zlim3d(-2.0, 2.0)
-plt.show()
-
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(1, 1, 1, projection='3d')
-ax1.scatter(x_test[:,0], x_test[:,1], y_pred)
-plt.title("Evolved Candidate Solutions")
-ax1.set_xlim3d(-100.0, 100.0)
-ax1.set_ylim3d(-100.0, 100.0)
-ax1.set_zlim3d(-2.0, 2.0)
-plt.show()
 
 accuracy = (tp + tn) / (tp + tn + fp + fn)
 recall = tp / (tp + fn + 0.00001)
